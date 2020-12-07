83f46f84223f788ea78d8fd7c9383d871aa84648
rubcv <spamdesareth@gmail.com>
2020-12-07 12:40:04 +0100

    Added new tests to Blockchain class

--- test/test.js
+++ test/test.js
@@ -6,8 +6,24 @@ const ALICE = "ALICE";
 const BOB = "BOB";
 
 console.log("Sending transaction from ALICE to BOB");
-console.log(salchichain.createNewTransaction(ALICE, BOB));
+const tx_1 = salchichain.createNewTransaction(ALICE, BOB);
+console.log(tx_1);
 
 console.log("Sending transaction from BOB to ALICE");
-console.log(salchichain.createNewTransaction(BOB, ALICE));
+const tx_2 = salchichain.createNewTransaction(BOB, ALICE);
+console.log(tx_2);
 
+console.log("Creating new block");
+salchichain.createNewBlock();
+
+console.log("Retrieving last block");
+const last_block = salchichain.getLastBlock();
+console.log(last_block);
+
+console.log("Searching by block_id...");
+const block = salchichain.getBlock(1);
+console.log(block);
+
+
+console.log("Searching tx_1...");
+console.log(salchichain.getTransaction(tx_1.tx_id, tx_1.block_id - 1));
